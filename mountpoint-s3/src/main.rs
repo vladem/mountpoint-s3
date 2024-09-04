3af9e21af4608ca67b0bb534e287f535d4f801d8
Vladislav Volodkin <vladvolodkin@gmail.com>
2024-09-04 20:19:00 +0000

    Revert "Use jemalloc allocator"
    This reverts commit b0ad94d95131ce39ca13fcc2a63b1961aa82b729.
    
    Signed-off-by: Vladislav Volodkin <vladvolodkin@gmail.com>

--- mountpoint-s3/src/main.rs
+++ mountpoint-s3/src/main.rs
@@ -1,6 +1,3 @@
-#[global_allocator]
-static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
-
 fn main() -> anyhow::Result<()> {
     mountpoint_s3::cli::main(mountpoint_s3::cli::create_s3_client)
 }
