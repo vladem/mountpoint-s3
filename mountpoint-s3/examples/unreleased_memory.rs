a84ad5381605a0a07bc6dfa3daa2549b788217a1
Vlad Volodkin <vlaad@amazon.com>
2025-01-24 13:59:00 +0000

    Run requests in parallel
    Signed-off-by: Vlad Volodkin <vlaad@amazon.com>

--- mountpoint-s3/examples/unreleased_memory.rs
+++ mountpoint-s3/examples/unreleased_memory.rs
@@ -1,7 +1,9 @@
+use std::ops::Range;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
 
 use clap::{Arg, Command};
+use futures::task::SpawnExt;
 use futures::StreamExt;
 use humansize::format_size;
 use mountpoint_s3_client::config::{EndpointConfig, S3ClientConfig};
@@ -9,11 +11,11 @@ use mountpoint_s3_client::types::GetObjectParams;
 use mountpoint_s3_client::{ObjectClient, S3CrtClient};
 use mountpoint_s3_crt::common::rust_log_adapter::RustLogAdapter;
 use regex::Regex;
+use std::time::Instant;
 use sysinfo::{get_current_pid, MemoryRefreshKind, ProcessRefreshKind, ProcessesToUpdate, System};
 use tracing_subscriber::fmt::Subscriber;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
-use std::time::Instant;
 
 /// Like `tracing_subscriber::fmt::init` but sends logs to stderr
 fn init_tracing_subscriber() {
@@ -27,10 +29,11 @@ fn init_tracing_subscriber() {
     subscriber.try_init().expect("unable to install global subscriber");
 }
 
-/// Creates requests sequentially and reads from each of them the specified number of bytes.
+/// Splits the given range in `requests-num` non-overlapping sub-ranges and spawns requests in parallel
+/// for each sub-range.
 ///
-/// Example usage (read 8MiB from 50 requests):
-/// cargo run --example unreleased_memory <BUCKET> <KEY> --range 0-8388608 --requests-num 50
+/// Example usage (read 1GiB from 128 requests, 8MiB each):
+/// cargo run --example unreleased_memory <BUCKET> <KEY> --range 0-1073741824 --requests-num 128
 fn main() {
     init_tracing_subscriber();
 
@@ -40,7 +43,7 @@ fn main() {
         .arg(Arg::new("key").required(true))
         .arg(Arg::new("region").long("region").default_value("us-east-1"))
         .arg(
-            Arg::new("requests")
+            Arg::new("requests-num")
                 .long("requests-num")
                 .default_value("1")
                 .value_parser(clap::value_parser!(u64)),
@@ -62,17 +65,20 @@ fn main() {
     let bucket = matches.get_one::<String>("bucket").unwrap();
     let key = matches.get_one::<String>("key").unwrap();
     let region = matches.get_one::<String>("region").unwrap();
-    let requests = matches.get_one::<u64>("requests").unwrap();
+    let requests_num = matches.get_one::<u64>("requests-num").unwrap();
     let part_size = matches.get_one::<u64>("part-size").unwrap();
     let part_size = *part_size as usize;
-    let range = matches.get_one::<String>("range").map(|s| {
-        let range_regex = Regex::new(r"^(?P<start>[0-9]+)-(?P<end>[0-9]+)$").unwrap();
-        let matches = range_regex.captures(s).expect("invalid range");
-        let start = matches.name("start").unwrap().as_str().parse::<u64>().unwrap();
-        let end = matches.name("end").unwrap().as_str().parse::<u64>().unwrap();
-        // bytes range is inclusive, but the `Range` type is exclusive, so bump the end by 1
-        start..(end + 1)
-    });
+    let range = matches
+        .get_one::<String>("range")
+        .map(|s| {
+            let range_regex = Regex::new(r"^(?P<start>[0-9]+)-(?P<end>[0-9]+)$").unwrap();
+            let matches = range_regex.captures(s).expect("invalid range");
+            let start = matches.name("start").unwrap().as_str().parse::<u64>().unwrap();
+            let end = matches.name("end").unwrap().as_str().parse::<u64>().unwrap();
+            // We treat the provided range as half-open: [start, end)
+            start..end
+        })
+        .expect("range is required");
 
     let config = S3ClientConfig::new()
         .endpoint_config(EndpointConfig::new(region))
@@ -86,63 +92,80 @@ fn main() {
     // let client = S3CrtClient::new(config).expect("couldn't create client");
 
     let start = Instant::now();
-    let start_clone = start.clone();
     let received_bytes_counter = Arc::new(AtomicU64::new(0));
     let received_bytes_counter_reader = received_bytes_counter.clone();
     let received_bytes_counter_writer = received_bytes_counter.clone();
     std::thread::spawn(move || {
         let mut sys = System::new();
         loop {
-            print_statistics(&mut sys, received_bytes_counter_reader.clone(), &start_clone);
+            print_statistics(&mut sys, received_bytes_counter_reader.clone());
             std::thread::sleep(std::time::Duration::from_secs(10));
         }
     });
     futures::executor::block_on(async move {
-        // Create `requests` requests sequentially
-        for _ in 0..*requests {
-            let mut request = client
-                .get_object(bucket, key, &GetObjectParams::new().range(range.clone()))
-                .await
-                .expect("couldn't create get request");
-            // Read from each request `range` bytes before dropping it and creating a new one
-            loop {
-                match StreamExt::next(&mut request).await {
-                    Some(Ok((_, body))) => {
-                        received_bytes_counter_writer.fetch_add(body.len() as u64, Ordering::SeqCst);
+        // Split the range to `requests` requests and execute them concurrently
+        let mut requests = vec![];
+        let request_ranges = get_request_ranges(range, *requests_num);
+        println!("request_ranges: {:?}", request_ranges);
+        for request_range in request_ranges {
+            let bucket = bucket.to_owned();
+            let key = key.to_owned();
+            let client = client.clone();
+            let received_bytes_counter_writer = received_bytes_counter_writer.clone();
+            let task = client.event_loop_group().spawn_with_handle(async move {
+                let mut request = client
+                    .get_object(&bucket, &key, &GetObjectParams::new().range(Some(request_range)))
+                    .await
+                    .expect("couldn't create get request");
+                // Read from each request till the end
+                loop {
+                    match StreamExt::next(&mut request).await {
+                        Some(Ok((_, body))) => {
+                            received_bytes_counter_writer.fetch_add(body.len() as u64, Ordering::SeqCst);
+                        }
+                        Some(Err(e)) => {
+                            tracing::error!(error = ?e, "request failed");
+                            break;
+                        }
+                        None => break,
                     }
-                    Some(Err(e)) => {
-                        tracing::error!(error = ?e, "request failed");
-                        break;
-                    }
-                    None => break,
                 }
-            }
+            });
+            requests.push(task.expect("spawn should succeed"));
         }
+
+        // wait for all requests to complete
+        for handle in requests {
+            handle.await;
+        }
+
         // explicitly drop the client
         drop(client);
     });
+
+    println!(
+        "bandwidth: {} MiB/s",
+        received_bytes_counter.load(Ordering::SeqCst) as f64 / start.elapsed().as_secs_f64() / 1024.0 / 1024.0
+    );
+
+    // It still takes sometime to observe the RSS decrease
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
     let mut sys = System::new();
-    print_statistics(&mut sys, received_bytes_counter, &start);
+    print_statistics(&mut sys, received_bytes_counter);
 }
 
-fn print_statistics(sys: &mut System, received_bytes_counter: Arc<AtomicU64>, start: &Instant) {
+fn print_statistics(sys: &mut System, received_bytes_counter: Arc<AtomicU64>) {
     println!(
         "read: {}",
-        format_size(received_bytes_counter.load(Ordering::SeqCst), humansize::DECIMAL)
+        format_size(received_bytes_counter.load(Ordering::SeqCst), humansize::BINARY)
     );
-    println!("bandwidth: {} MiB/s", received_bytes_counter.load(Ordering::SeqCst) as f64 / start.elapsed().as_secs_f64() / 1024.0 / 1024.0);
     unsafe {
         let mallinfo = libc::mallinfo();
-        println!("arena: {}", format_size(mallinfo.arena as u32, humansize::DECIMAL)); /* non-mmapped space allocated from system */
-        println!("hblkhd: {}", format_size(mallinfo.hblkhd as u32, humansize::DECIMAL)); /* space in mmapped regions */
-        println!(
-            "fordblks: {}",
-            format_size(mallinfo.fordblks as u32, humansize::DECIMAL)
-        ); /* total free space */
-        println!(
-            "keepcost: {}",
-            format_size(mallinfo.keepcost as u32, humansize::DECIMAL)
-        );
+        println!("arena: {}", format_size(mallinfo.arena as u32, humansize::BINARY)); /* non-mmapped space allocated from system */
+        println!("hblkhd: {}", format_size(mallinfo.hblkhd as u32, humansize::BINARY)); /* space in mmapped regions */
+        println!("fordblks: {}", format_size(mallinfo.fordblks as u32, humansize::BINARY)); /* total free space */
+        println!("keepcost: {}", format_size(mallinfo.keepcost as u32, humansize::BINARY));
     }
     if let Ok(pid) = get_current_pid() {
         sys.refresh_memory_specifics(MemoryRefreshKind::nothing().with_ram());
@@ -152,8 +175,18 @@ fn print_statistics(sys: &mut System, received_bytes_counter: Arc<AtomicU64>, st
             ProcessRefreshKind::nothing().with_memory(),
         );
         if let Some(process) = sys.process(pid) {
-            println!("rss: {}", format_size(process.memory(), humansize::DECIMAL));
+            println!("rss: {}", format_size(process.memory(), humansize::BINARY));
         }
     }
     println!("___");
+}
+
+fn get_request_ranges(download_range: Range<u64>, requests_num: u64) -> Vec<Range<u64>> {
+    let total_bytes = download_range.end - download_range.start;
+    let step = total_bytes / requests_num;
+    let download_range_end = download_range.end;
+    download_range
+        .step_by(step as usize)
+        .map(|start| (start..(start + step).min(download_range_end)))
+        .collect()
 }
